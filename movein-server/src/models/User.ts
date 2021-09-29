0850e0eee4edaecb4adba99fc23a697c72b3e547
יאיר מאמנים <yairmeamnim@gmail.com>
2021-09-29 20:20:44 +0300

    FEAT: added a User Model

--- movein-server/src/models/User.ts
+++ movein-server/src/models/User.ts
@@ -1,5 +1,33 @@
-const Sequelize = require('sequelize')
-import sequelize from '../util/database';
+import Sequelize from "sequelize";
+import db from "../util/database";
 
+const User = db.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  userName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+    allowNull: false,
+  },
+});
 
-export { }
+export default User;
