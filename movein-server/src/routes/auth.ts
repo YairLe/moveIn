423224c3c34a7e0f67fd4db110586a7e978d6e6f
יאיר מאמנים <yairmeamnim@gmail.com>
2021-09-29 22:01:10 +0300

    FEAT: added an auth routes- signUp and login

---
+++ movein-server/src/routes/auth.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { loginUser, signupUser } from "../controllers/auth";
+import { body } from "express-validator";
+import User from "../models/user";
+
+const router = express.Router();
+
+router.post(
+  "/signup",
+  [
+    body("userName").custom((value, { req }) => {
+      User.findOne({ where: { userName: value } }).then((userDoc: Object) => {
+        if (userDoc) {
+          return Promise.reject("userName already exists");
+        }
+      });
+    }),
+    body("password").trim().isLength({ min: 5 }),
+  ],
+  signupUser,
+);
+
+router.post("/login", loginUser);
+
+export default router;
