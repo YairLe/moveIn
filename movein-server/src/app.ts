fbbba8f03ac4a583f15f4897255b52d61a50a707
yairmeamnim <yairmeamnim@gmail.com>
2021-09-29 15:40:17 +0300

    WIP: started working on adding sequelize

--- movein-server/src/app.ts
+++ movein-server/src/app.ts
@@ -1,7 +1,17 @@
+
 import express from 'express';
+// import exphbs=require('express-handlebars')
+import bodyParser from 'body-parser';
+import path from 'path';
+import cors from 'cors';
+import sequelize from './util/database';
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send('hello from my app');
