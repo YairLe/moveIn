a8d0d013d9b486410eb44b9234bb2db240077aec
יאיר מאמנים <yairmeamnim@gmail.com>
2021-09-29 20:18:25 +0300

    FEAT: init sequelize db

--- movein-server/src/app.ts
+++ movein-server/src/app.ts
@@ -1,22 +1,25 @@
-
-import express from 'express';
+import express from "express";
 // import exphbs=require('express-handlebars')
-import bodyParser from 'body-parser';
-import path from 'path';
-import cors from 'cors';
-import sequelize from './util/database';
-
+import bodyParser from "body-parser";
+import path from "path";
+import cors from "cors";
+import db from "./util/database";
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(cors());
 
+db.authenticate()
+  .then(() => {
+    console.log("DataBase connected");
+  })
+  .catch((err: any) => console.error(err));
 
-app.get('/', (req, res) => {
-    res.send('hello from my app');
+app.get("/", (req, res) => {
+  res.send("hello from my app");
 });
 
 app.listen(port, () => {
-    console.log(`server is listening on ${port}`);
+  console.log(`server is listening on ${port}`);
 });
