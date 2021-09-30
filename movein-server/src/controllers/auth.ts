1ea8f4e2fc29d5c29115880e854fef935fb96416
יאיר מאמנים <yairmeamnim@gmail.com>
2021-09-30 15:40:20 +0300

    REF: added a general error

--- movein-server/src/controllers/auth.ts
+++ movein-server/src/controllers/auth.ts
@@ -1,9 +1,10 @@
+import bcrypt from "bcryptjs";
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
 import errorCode from "../errors/errorCode";
+import generalError from "../errors/generalError";
 import User from "../models/user";
-const { validationResult } = require("express-validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-import { Request, Response, NextFunction } from "express";
 
 export const signupUser = async (
   req: Request,
@@ -12,8 +13,7 @@ export const signupUser = async (
 ) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error("Validation failed") as any;
-    error.statusCode = 422;
+    const error = generalError("Validation failed", 422);
     error.data = errors.array();
     throw error;
   }
@@ -27,7 +27,6 @@ export const signupUser = async (
       updatedAt: new Date(),
     });
     const result = await user.save();
-    console.log(result);
     res.status(201).json({ message: "User created!", userId: result.id });
   } catch (err) {
     errorCode(err, next);
@@ -43,18 +42,12 @@ export const loginUser = async (
   try {
     const user = await User.findOne({ where: { userName: userName } });
     if (!user) {
-      const error = new Error(
-        "A user with this email could not be found",
-      ) as any;
-      error.statusCode = 401;
-      throw error;
+      throw generalError("A user with this email could not be found", 401);
     }
     const isEqual = await bcrypt.compare(password, user.password);
 
     if (!isEqual) {
-      const error = new Error("wrong password") as any;
-      error.statusCode = 401;
-      throw error;
+      throw generalError("wrong password", 401);
     }
     const token = jwt.sign(
       {
