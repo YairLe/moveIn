6b9527a4bc50c8b933d008f6b4d251671882eb13
יאיר מאמנים <yairmeamnim@gmail.com>
2021-10-07 17:38:50 +0300

    FEAT: first init of client auth

---
+++ movein-client/src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface IProps {
+  children: React.ReactNode;
+  buttonProp?: React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >;
+}
+
+const Button: React.FC<IProps> = (props: IProps) => {
+  const { children, buttonProp } = props;
+  return <button {...buttonProp}>{children}</button>;
+};
+
+export default Button;
