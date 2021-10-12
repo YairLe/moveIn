c30b8ec37a8bf16769dfdba0b2712593dbfe75d5
יאיר מאמנים <yairmeamnim@gmail.com>
2021-10-12 18:30:14 +0300

    FEAT: added a navigation and routing

--- movein-client/src/App.tsx
+++ movein-client/src/App.tsx
@@ -1,12 +1,40 @@
 import React from "react";
-import logo from "./images/MoveIn.svg";
-import "./App.css";
 import Auth from "./components/Auth/Auth";
+import { Redirect, Route, Switch, useLocation } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import Requirements from "./pages/Requirements";
 
 function App() {
+  const isAuth = true;
+  const location = useLocation();
   return (
     <div>
-      <Auth />
+      <Switch>
+        {isAuth ? (
+          <>
+            <Route path="/main">
+              <MainPage />
+            </Route>
+            <Route path="/requirements">
+              <Requirements />
+            </Route>
+            <Route path="/apartments">
+              <MainPage />
+            </Route>
+            {/* <Route path="*">
+              <Redirect to="/main" />
+            </Route> */}
+          </>
+        ) : (
+          <Route path="/" exact>
+            <Auth />
+          </Route>
+        )}
+
+        <Route path="*">
+          <Redirect to={isAuth ? "/main" : "/"} />
+        </Route>
+      </Switch>
     </div>
   );
 }
