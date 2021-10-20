4fa2725724816eb300763ecb5996c3d202026e23
יאיר מאמנים <yairmeamnim@gmail.com>
2021-10-20 17:29:31 +0300

    FEAT: added inheritance routes for requirements

--- movein-client/src/App.tsx
+++ movein-client/src/App.tsx
@@ -1,41 +1,70 @@
 import React from "react";
 import Auth from "./components/Auth/Auth";
-import { Redirect, Route, Switch, useLocation } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import Requirements from "./pages/Requirements";
+import EditPrice from "./components/Requirements/Price/EditPrice";
+import { EditProvider } from "./context/EditContext";
+import EditArea from "./components/Requirements/Area/EditArea";
+import EditRooms from "./components/Requirements/Rooms/EditRooms";
+import EditEssentials from "./components/Requirements/Essentials/EditEssentials";
 
 function App() {
   const isAuth = true;
-  const location = useLocation();
-  return (
-    <div>
-      <Switch>
-        {isAuth ? (
-          <>
-            <Route path="/main">
-              <MainPage />
-            </Route>
-            <Route path="/requirements">
-              <Requirements />
-            </Route>
-            <Route path="/apartments">
-              <MainPage />
-            </Route>
-            <Route path="/" exact>
-              <Redirect to="/main" />
-            </Route>
-          </>
-        ) : (
-          <Route path="/">
-            <Auth />
-          </Route>
-        )}
 
-        <Route path="*">
-          <Redirect to={isAuth ? "/main" : "/"} />
+  const RequirementsRoutes = () => {
+    return (
+      <EditProvider>
+        <Switch>
+          <Route path="/requirements/price">
+            <EditPrice />
+          </Route>
+          <Route path="/requirements/area">
+            <EditArea />
+          </Route>
+          <Route path="/requirements/rooms">
+            <EditRooms />
+          </Route>
+          <Route path="/requirements/essentials">
+            <EditEssentials />
+          </Route>
+          <Route path="*">
+            <Requirements />
+            <Redirect to="/requirements" />
+          </Route>
+        </Switch>
+      </EditProvider>
+    );
+  };
+
+  return (
+    <Switch>
+      {isAuth ? (
+        <Switch>
+          <Route exact path="/main">
+            <MainPage />
+          </Route>
+          <Route path="/requirements">{RequirementsRoutes()}</Route>
+          <Route exact path="/apartments">
+            <MainPage />
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/main" />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      ) : (
+        <Route path="/">
+          <Auth />
         </Route>
-      </Switch>
-    </div>
+      )}
+
+      <Route exact path="*">
+        <Redirect to="/" />
+      </Route>
+    </Switch>
   );
 }
 
