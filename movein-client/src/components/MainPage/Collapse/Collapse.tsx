4e80c99ef3f8438b22dbf0d6db6cbda2173adc4c
יאיר מאמנים <yairmeamnim@gmail.com>
2021-10-20 17:52:14 +0300

    FEAT: added a cube-button generic, and added a collapsing view

--- movein-client/src/components/MainPage/Collapse/Collapse.tsx
+++ movein-client/src/components/MainPage/Collapse/Collapse.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { EditContext } from "../../../context/EditContext";
+import EditButton from "../../Button/EditButton";
 import Cube from "../Cube/Cube";
 import styles from "./Collapse.module.css";
 interface IProps {
@@ -12,20 +15,35 @@ interface IProps {
 const Collapse: React.FC<IProps> = (props: IProps) => {
   const { collapsed, children, cubeName, cubeButtonStyle, cubeDivStyle } =
     props;
+  const { isEdit: editState } = useContext(EditContext);
   const [isCollapsed, setIsCollapsed] = React.useState(collapsed);
 
+  const history = useHistory();
   return (
     <>
-      <Cube
-        cubeName={cubeName}
-        divStyle={cubeDivStyle}
-        buttonStyle={`${cubeButtonStyle} ${
-          isCollapsed ? styles.buttonCollapsed : styles.buttonExpanded
-        }`}
-        onClickButton={() => {
-          setIsCollapsed(!isCollapsed);
-        }}
-      />
+      <div className={styles.divContainer}>
+        <div className={styles.divCube}>
+          <Cube
+            cubeName={cubeName}
+            divStyle={cubeDivStyle}
+            buttonStyle={`${cubeButtonStyle} ${
+              isCollapsed ? styles.buttonCollapsed : styles.buttonExpanded
+            }`}
+            onClickButton={() => {
+              setIsCollapsed(!isCollapsed);
+            }}
+          />
+        </div>
+        <div className={styles.divButton}>
+          {editState && (
+            <EditButton
+              handleClick={() => {
+                history.push(`/requirements/${cubeName.toLowerCase()}`);
+              }}
+            />
+          )}
+        </div>
+      </div>
 
       <div
         className={isCollapsed ? styles.collapsed : styles.expanded}
--- movein-client/src/components/MainPage/Cube/Cube.tsx
+++ movein-client/src/components/MainPage/Cube/Cube.tsx
@@ -7,16 +7,27 @@ interface IProps {
   divStyle?: string;
   buttonStyle?: string;
   onClickButton?: Function;
+  buttonType?: "button" | "submit" | "reset";
+  buttonDisabled?: boolean;
 }
 
 const Cube: React.FC<IProps> = (props: IProps) => {
-  const { cubeName, divStyle, buttonStyle, onClickButton = () => {} } = props;
+  const {
+    cubeName,
+    divStyle,
+    buttonType = "button",
+    buttonStyle,
+    buttonDisabled = false,
+    onClickButton = () => {},
+  } = props;
   return (
     <div className={divStyle}>
       <Button
         buttonProp={{
           className: `${styles.button} ${buttonStyle}`,
           onClick: () => onClickButton(),
+          type: buttonType,
+          disabled: buttonDisabled,
         }}
       >
         {cubeName}
