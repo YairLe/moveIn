788c7d49025fc19a1b3415c0d1d28367ac91803b
יאיר מאמנים <yairmeamnim@gmail.com>
2021-10-25 11:14:09 +0300

    new heroku commit

--- movein-server/dist/app.js
+++ movein-server/dist/app.js
@@ -11,24 +11,30 @@ const Requirements_1 = __importDefault(require("./models/Requirements"));
 const User_1 = __importDefault(require("./models/User"));
 const auth_1 = __importDefault(require("./routes/auth"));
 const requirements_1 = __importDefault(require("./routes/requirements"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
 app.use(body_parser_1.default.json());
 app.use(body_parser_1.default.urlencoded({ extended: true }));
-app.use((0, cors_1.default)());
+const corsOptions = {
+    allowedHeaders: ["Content-Type", "Authorization"],
+};
+// app.use(express.static(path.join(__dirname, "../movein-client/build")));
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../movein-client", "build")));
 app.use(auth_1.default);
 app.use(is_auth_1.default, requirements_1.default);
 User_1.default.hasOne(Requirements_1.default);
 Requirements_1.default.belongsTo(User_1.default);
-app.get("/", (req, res) => {
-    res.send("hello from my app");
+app.use(express_1.default.static("public"));
+app.use((req, res, next) => {
+    res.sendFile(path_1.default.join(__dirname, "../../movein-client", "build", "index.html"));
 });
 app.use((error, req, res, next) => {
-    console.log(error, " i am also here");
     const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
-    res.status(status).json({ message: message, data: data });
+    res.status(status).json({ message, data });
 });
 app.listen(port, () => {
     console.log(`server is listening on ${port}`);
--- movein-server/src/app.ts
+++ movein-server/src/app.ts
@@ -6,7 +6,7 @@ import Requirements from "./models/Requirements";
 import User from "./models/User";
 import authRoute from "./routes/auth";
 import requirementsRoute from "./routes/requirements";
-
+import path from "path";
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -15,18 +15,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"],
 };
+// app.use(express.static(path.join(__dirname, "../movein-client/build")));
 app.use(cors(corsOptions));
 
+app.use(express.static(path.join(__dirname, "../../movein-client", "build")));
 app.use(authRoute);
 app.use(checkAuth, requirementsRoute);
 
 User.hasOne(Requirements);
 Requirements.belongsTo(User);
 
-app.get("/", (req, res) => {
-  res.send("hello from my app");
-});
+app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.sendFile(
+    path.join(__dirname, "../../movein-client", "build", "index.html"),
+  );
+});
 app.use((error: any, req: any, res: any, next: any) => {
   const status = error.statusCode || 500;
   const message = error.message;
