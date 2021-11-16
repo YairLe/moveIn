e60e5793173311e75f543bc747a2dc8e27a9a430
יאיר מאמנים <yairmeamnim@gmail.com>
2021-11-16 16:47:32 +0200

    FEAT: added slider for third page with images preview

--- movein-server/movein-client/src/components/Apartments/NewApartmentPages/NewApartmentThirdPage.tsx
+++ movein-server/movein-client/src/components/Apartments/NewApartmentPages/NewApartmentThirdPage.tsx
@@ -1,164 +1,47 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useCookies } from "react-cookie";
 import { NewApartmentContext } from "../../../context/NewApartmentContext";
-import UseAxios from "../../../hooks/use-axios";
-import useInput from "../../../hooks/use-input";
-import SaveButton from "../../Button/SaveButton";
 import Input from "../../Input/Input";
-import IncreasedInput from "../../Requirements/IncreasedInput";
-import InputTextForm from "../InputTextForm";
 import styles from "../NewApartment.module.css";
+import Slider from "./Slider";
 
 const NewApartmentThirdPage: React.FC = () => {
   const { newApartment, setNewApartment } = useContext(NewApartmentContext);
 
-  const [inputList, setInputList] = useState<string[]>(newApartment.comments);
-  const [imgValue, setImgValue] = useState<any>();
-  const validator = (value: string) => /^\d+$/.test(value) && +value > 0;
-
-  const [cookies] = useCookies(["token"]);
-
-  const { loading, fetchData } = UseAxios({
-    method: "post",
-    url: "/upload",
-  });
-
-  const getdata = async () => {
-    console.log(imgValue, "the form data is");
-    const formData = new FormData();
-    formData.append("files", imgValue);
-    const response = await fetchData(formData, {
-      Authorization: `Bearer ${cookies.token}`,
-      // encType: "multipart/form-data",
-      ContentType: `multipart/form-data`,
-      // "Content-Type": "multipart/form-data",
-      // "Content-type": "application/json",
-    });
-    // console.log(response.data);
-    // if (response.data) {
-    //   switch (response.data.message) {
-    //     case "No requirements found for user":
-    //       alert("make sure to insert data!");
-    //       break;
-    //     case "User requirement retrived":
-    //       localStorage.setItem(
-    //         "requirements",
-    //         JSON.stringify(response.data.data)
-    //       );
-
-    //       setRequirements(response.data.data);
-    //       break;
-    //   }
-    // }
-  };
-
-  //   const {
-  //     value,
-  //     isValid,
-  //     notifyInvalidValue,
-  //     inputSettingsChangeHandler,
-  //     inputBlur,
-  //   } = useInput(() => {}, "");
-
-  //   const inputProp = {
-  //     type: "file",
-  //     id: "fileUpload",
-  //     multiple: true,
-  //     // name: name,
-  //     value: value,
-  //     onChange: inputSettingsChangeHandler,
-  //     onBlur: inputBlur,
-  //     // min: 0,
-  //   };
+  const [imgValue, setImgValue] = useState<any>(
+    newApartment.photos !== "empty" ? newApartment.photos : null,
+  );
 
   const changeFile: React.ChangeEventHandler<HTMLInputElement> = (event) => {
-    console.log(event, "printing event");
     const element = { ...newApartment };
     element.photos = event.target.value;
-    // setNewApartment(element);
-    console.log(
-      event.target.files ? event.target.files[0] : null,
-      "changed file"
-    );
-    setImgValue(event.target.files ? event.target.files[0] : null);
-    // setInputSettings((prevState) => {
-    //   return { ...prevState, enteredValue: event.target.value };
-    // });
+    setImgValue(event.target.files ? event.target.files : {});
+  };
+
+  const inputProp = {
+    type: "file",
+    name: "pic",
+    id: "fileUpload",
+    multiple: true,
+    onChange: changeFile,
+    style: { color: "transparent" },
   };
 
   useEffect(() => {
-    console.log(imgValue);
-    if (imgValue) {
-      console.log(imgValue, "the image value is");
-      getdata();
-    }
+    const element = { ...newApartment };
+    element.photos = imgValue && imgValue.length > 0 ? imgValue : "empty";
+    setNewApartment(element);
   }, [imgValue]);
 
-  console.log(newApartment);
-  //   console.log("value is", value);
   return (
     <React.Fragment>
-      {/* <form
-        action="http://localhost:8080/upload"
-        encType="multipart/form-data"
-        method="POST"
-      >
-        <input type="file" name="pic" />
-        <input type="submit" value="Upload a file" />
-      </form> */}
-      <form
-        // action="http://localhost:8080/upload"
-        // encType="multipart/form-data"
-        // method="POST"
-        onSubmit={(event) => {
-          event.preventDefault();
-          // getdata();
-        }}
-      >
-        <div className={styles.inputDiv}>
-          <input
-            type="file"
-            name="pic"
-            multiple={false}
-            // value={imgValue}
-            id="fileUpload"
-            onChange={changeFile}
-          />
-        </div>
-        <SaveButton buttonDisabled={false} />
-      </form>
-
-      {/* <div className={styles.inputDivSecondPage}>
-        <InputTextForm
-          {...inputTextProp}
-          id="FloorMin"
-          name="FloorMin"
-          label="Floor"
-          type="number"
-          inputStyle={styles.inputFloor}
-          labelStyle={styles.labelFloor}
+      <div className={styles.inputDiv}>
+        <Input
+          inputInValid={false}
+          inputInValidText={""}
+          inputProp={inputProp}
         />
-        <InputTextForm
-          {...inputTextProp}
-          id="FloorMax"
-          name="FloorMax"
-          label="out of"
-          type="number"
-          inputStyle={styles.inputFloor}
-          labelStyle={styles.labelFloor}
-        />
-      </div> */}
-
-      {/* <IncreasedInput
-        inputList={inputList}
-        setInputList={setInputList}
-        placeholder="Add Comments"
-        element={
-          <p style={{ paddingLeft: ".5rem" }} className={styles.label}>
-            Comments
-          </p>
-        }
-      /> */}
+      </div>
+      {imgValue && imgValue.length > 0 && <Slider imgValue={imgValue} />}
     </React.Fragment>
   );
 };
---
+++ movein-server/movein-client/src/components/Apartments/NewApartmentPages/Slider.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import SwiperCore, { Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.css";
+SwiperCore.use([Pagination]);
+
+interface IProps {
+  imgValue: any;
+}
+
+const Slider: React.FC<IProps> = (props: IProps) => {
+  const { imgValue } = props;
+  return (
+    <React.Fragment>
+      <Swiper
+        centeredSlides={true}
+        className="mySwiper"
+        style={{
+          width: "100vw",
+          marginBottom: "1rem",
+        }}
+        loop={true}
+        spaceBetween={300}
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+        }}
+      >
+        {Object.keys(imgValue).map((key) => {
+          return (
+            <SwiperSlide
+              style={{
+                height: "25rem",
+                justifyContent: "center",
+              }}
+              key={`slide-${key}`}
+            >
+              <img
+                src={URL.createObjectURL(imgValue[key])}
+                style={{
+                  listStyle: "none",
+                  width: "100%",
+                }}
+                alt={`Slide ${key}`}
+              />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </React.Fragment>
+  );
+};
+
+export default Slider;
