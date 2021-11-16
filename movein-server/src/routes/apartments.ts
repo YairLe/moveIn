5e155a69444defe2bb3dcc33c5f65e65523aee82
יאיר מאמנים <yairmeamnim@gmail.com>
2021-11-16 16:33:59 +0200

    FIX: removed unused comments

--- movein-server/src/routes/apartments.ts
+++ movein-server/src/routes/apartments.ts
@@ -1,28 +1,8 @@
 import express from "express";
 import { addNewApartment } from "../controllers/apartments";
-import { body } from "express-validator";
-import User from "../models/User";
 
 const router = express.Router();
 
-
-router.post('/newApartment', addNewApartment);
-
-// router.post(
-//   "/signup",
-//   [
-//     body("userName").custom((value) => {
-//       return User.findOne({ where: { userName: value } }).then((userDoc: Object) => {
-//         if (userDoc) {
-//           return Promise.reject("userName already exists");
-//         }
-//       });
-//     }),
-//     body("password").trim().isLength({ min: 5 }),
-//   ],
-//   signupUser,
-// );
-
-// router.post("/login", loginUser);
+router.post("/newApartment", addNewApartment);
 
 export default router;
