7b60162b6745ab0b1d62bbab1b784325313719b7
YairLe <yairmeamnim@gmail.com>
2021-11-25 15:26:34 +0200

    WIP: started adding apartmentView page

--- movein-server/movein-client/src/components/Apartments/NewApartmentPages/ApartmentView.tsx
+++ movein-server/movein-client/src/components/Apartments/NewApartmentPages/ApartmentView.tsx
@@ -2,7 +2,9 @@ import React, { useEffect } from "react";
 import { useCookies } from "react-cookie";
 import { useParams } from "react-router-dom";
 import UseAxios from "../../../hooks/use-axios";
-
+import Header from "../../Header/Header";
+import HeaderComponent from "../../Requirements/HeaderComponent";
+import styles from "./ApartmentView.module.css";
 interface IApartmentParams {
   apartmentId: string;
 }
@@ -11,6 +13,19 @@ const ApartmentView: React.FC = () => {
   const [cookies] = useCookies(["token"]);
   const apartmentParams: IApartmentParams = useParams();
   console.log(apartmentParams.apartmentId);
+  const apartmentData = {
+    street: "Saifan",
+    neighborhood: "Ramat almogi",
+    city: "Haifa",
+    images: [],
+    rent: 3000,
+    tax: 1200,
+    committe: 120,
+    rooms: 4,
+    floorMin: 0,
+    floorMax: 5,
+    comments: [""],
+  };
   const { loading, fetchData } = UseAxios({
     method: "get",
     url: `/getUserApartments/${apartmentParams.apartmentId}`,
@@ -40,7 +55,41 @@ const ApartmentView: React.FC = () => {
     // getData();
   }, []);
 
-  return <div></div>;
+  return (
+    <React.Fragment>
+      <Header
+        headerStyle={styles.header}
+        element={
+          <div
+            style={{
+              paddingTop: ".5rem",
+              paddingLeft: ".6rem",
+              display: "flex",
+            }}
+          >
+            <HeaderComponent
+              editButtonStyle={styles.editButton}
+              headerStyle={styles.div}
+            />
+            <div
+              style={{
+                display: "flex",
+                paddingLeft: "1rem",
+                // justifyContent: "center",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <b className={styles.label}>{apartmentData.street}</b>
+              <b className={styles.label}>{apartmentData.neighborhood}</b>
+              <b className={styles.label}>{apartmentData.city}</b>
+            </div>
+          </div>
+        }
+      />
+      <div>hey bro</div>
+    </React.Fragment>
+  );
 };
 
 export default ApartmentView;
